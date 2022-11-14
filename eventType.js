acae72f271a32f5776f206e4177f0daf7605e55f
Theophilus Okwugwuni <theophilusdev@gmail.com>
2022-11-14 02:26:30 +0100

    initial commit

---
+++ eventType.js
@@ -0,0 +1,12 @@
+import avro from "avsc";
+
+export default avro.Type.forSchema({
+  type: "record",
+  fields: [
+    {
+      name: "category",
+      type: { type: "enum", symbols: ["CAT", "DOG"] },
+    },
+    { name: "noise", type: "string" },
+  ],
+});
